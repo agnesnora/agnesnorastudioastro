534bbbf26479078bf75749a8c1d22f0b344af953
agnesnora <agnesnora@gmail.com>
2026-02-26 15:25:31 +0100

    blog sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,21 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://agnesnorastudio.hu',
-  integrations: [react(), icon(), sitemap()],
+  integrations: [
+    react(),
+    icon(),
+    sitemap(),
+    sanity({
+      projectId: 'a6fqp12a',
+      dataset: 'production',
+      useCdn: false, // for static builds
+    }),
+  ],
 
   vite: {
     plugins: [tailwindcss()],

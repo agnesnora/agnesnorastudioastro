d773601725a504c65e5c2abef0b0223013d585c9
agnesnora <agnesnora@gmail.com>
2026-02-26 15:25:31 +0100

    blog sanity

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,10 @@ declare global {
     gtag: (...args: unknown[]) => void;
   }
 }
+/// <reference types="astro/client" />
+/// <reference types="@sanity/astro/module" />
+declare module 'sanity:client' {
+  import type { SanityClient } from '@sanity/client';
+  const client: SanityClient;
+  export { client as sanityClient };
+}
